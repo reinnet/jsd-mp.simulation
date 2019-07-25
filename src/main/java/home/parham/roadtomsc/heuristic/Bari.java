f805841a6bdf9686e9e1df55818317b5868c4280
Parham Alvani <parham.alvani@gmail.com>
2019-07-26 02:34:54 +0430

    feat: Complete Bari Placement

--- src/main/java/home/parham/roadtomsc/heuristic/Bari.java
+++ src/main/java/home/parham/roadtomsc/heuristic/Bari.java
@@ -18,31 +18,50 @@ import home.parham.roadtomsc.domain.Node;
 import home.parham.roadtomsc.domain.Types;
 import home.parham.roadtomsc.problem.Config;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Bari {
     private Config cfg;
+    private ArrayList<ArrayList<Integer>> placement;
 
     public Bari(Config cfg) {
         this.cfg = cfg;
+        this.placement = new ArrayList<>();
     }
 
+    // place places the given chain with Bari algorithm
     public void place(int index) {
         Chain chain = this.cfg.getChains().get(index);
+        // chain placement array that maps each vnf to its physical node
+        ArrayList<Integer> placement = new ArrayList<>();
 
         // each node represents the stage in Bari multi-stage graph
         for (int stage = 0; stage < chain.nodes() - 1; stage++) {
             final Types.Type t = chain.getNode(stage);
+
+            // list the available nodes
             List<Node> nodes = this.cfg.getNodes().stream()
                     .filter(node -> !t.isIngress() || node.isIngress()) // ingress
                     .filter(node -> node.getCores() >= t.getCores()) // number of cores
                     .filter(node -> node.getRam() >= t.getRam()) // amount of ram
                     .filter(node -> !t.isIngress() || node.isEgress()) // egress
                     .collect(Collectors.toList());
+            // provide a simple integer as score for each node
+            Map<Integer, Integer> scores = nodes.stream().collect(Collectors.toMap(
+                    n -> this.cfg.getNodeIndex(n.getName()),
+                    n -> 0
+            ));
+
             if (stage > 1) { // use bfs from the last placed node
+                int selectedNode = placement.get(stage - 1);
             }
+            int bestNode;
             // select a physical node with minimum cost
+            placement.add(bestNode);
         }
+
+
+        this.placement.add(placement);
     }
 }
