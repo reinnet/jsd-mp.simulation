b3db6cdf63aaa552ca0d21951ed0438aaa90667a
Parham Alvani <parham.alvani@gmail.com>
2019-07-26 11:36:54 +0430

    fix: Correct BFS Algorithm

--- src/main/java/home/parham/roadtomsc/heuristic/Bari.java
+++ src/main/java/home/parham/roadtomsc/heuristic/Bari.java
@@ -13,6 +13,7 @@
 
 package home.parham.roadtomsc.heuristic;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import home.parham.roadtomsc.domain.Chain;
 import home.parham.roadtomsc.domain.Node;
 import home.parham.roadtomsc.domain.Types;
@@ -24,24 +25,35 @@ import java.util.stream.Collectors;
 
 public class Bari {
     private Config cfg;
+
+    public List<List<Integer>> getPlacement() {
+        return Collections.unmodifiableList(
+                this.placement.stream().map(Collections::unmodifiableList).collect(Collectors.toList())
+        );
+    }
+
     private ArrayList<ArrayList<Integer>> placement;
 
     public Bari(Config cfg) {
         this.cfg = cfg;
         this.placement = new ArrayList<>();
+
+        this.cfg.getChains().forEach(this::place);
     }
 
     /**
      * place places the given chain with Bari algorithm
-     * @param index of the given chain in configuration
+     * @param chain for placement
      */
-    public void place(int index) {
-        Chain chain = this.cfg.getChains().get(index);
+    private void place(Chain chain) {
+        System.out.printf(">> place chain with %d nodes\n", chain.nodes());
+
         // chain placement array that maps each vnf to its physical node
         ArrayList<Integer> placement = new ArrayList<>();
 
         // each node represents the stage in Bari multi-stage graph
         for (int stage = 0; stage < chain.nodes() - 1; stage++) {
+            System.out.printf(">> stage (%d)\n", stage);
             final Types.Type t = chain.getNode(stage);
 
             // list the available nodes
@@ -57,7 +69,7 @@ public class Bari {
                     n -> 0
             ));
 
-            if (stage > 1) { // use bfs from the last placed node
+            if (stage > 0) { // use bfs from the last placed node
                 int selectedNode = placement.get(stage - 1);
                 this.bfs(selectedNode, scores);
             }
@@ -81,13 +93,20 @@ public class Bari {
      */
     private void bfs(int root, Map<Integer, Integer> scores) {
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        Map<Integer, Boolean> seen = new HashMap<>();
+
         q.add(new Pair<>(root, 0));
 
         while (!q.isEmpty()) {
-            Pair<Integer, Integer> p = q.peek();
+            Pair<Integer, Integer> p = q.remove();
             int source = p.getKey();
             int depth = p.getValue();
+
+            if (!seen.getOrDefault(source, false))
+                continue;
+
             scores.computeIfPresent(source, (node, score) -> depth);
+            seen.put(source, true);
 
             this.cfg.getLinks().stream()
                     .filter(l -> l.getSource() == source)
