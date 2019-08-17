14fd994c68a9e3fac6acf2164f14b99e57057a47
Parham Alvani <parham.alvani@gmail.com>
2019-08-17 23:56:01 +0430

    feat: Add Solving Method Interface

---
+++ src/main/java/home/parham/roadtomsc/problem/Method.java
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ======================================
+ * [] Project Name : roadtomsc
+ *
+ * [] Package Name : home.parham.roadtomsc.problem
+ *
+ * [] Creation Date : 17-08-2019
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+ */
+
+package home.parham.roadtomsc.problem;
+
+public interface Method {
+    public Solution solve();
+}
